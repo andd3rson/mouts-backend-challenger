0dc53ab4d4b38c9fb7d35bc615c9154be5b91277
anderson conceicao <andersonconceiicao@gmail.com>
2025-09-29 16:32:39 -0300

    Added unit test to sales handlers  in application layer

---
+++ tests/Ambev.DeveloperEvaluation.Unit/Application/SalesTests/CreateSaleHandlerTests.cs
@@ -0,0 +1,57 @@
+﻿namespace Ambev.DeveloperEvaluation.Unit.Application.SalesTests;
+public class CreateSaleHandlerTests
+{
+    private readonly ISaleRepository _repo = Substitute.For<ISaleRepository>();
+    private readonly IMapper _mapper = Substitute.For<IMapper>();
+    private readonly ILogger<CreateSaleHandler> _logger = Substitute.For<ILogger<CreateSaleHandler>>();
+    private readonly CreateSaleHandler _handler;
+
+    public CreateSaleHandlerTests()
+    {
+        _handler = new CreateSaleHandler(_repo, _mapper, _logger);
+    }
+
+    [Fact]
+    public async Task Handle_ValidRequest_ReturnsCreateSaleResult()
+    {
+        // Arrange
+        var command = SaleHandlerTestData.ValidCreateSaleCommand();
+        var saleEntity = new Sale
+        {
+            Id = Guid.NewGuid(),
+            CustomerId = command.CustomerId,
+            BranchId = command.BranchId           
+        };
+        var expected = new CreateSaleResult { Id = saleEntity.Id };
+
+        _mapper.Map<Sale>(command).Returns(saleEntity);
+        _repo.CreateAsync(saleEntity, Arg.Any<CancellationToken>())
+             .Returns(saleEntity);
+        _mapper.Map<CreateSaleResult>(saleEntity).Returns(expected);
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Id.Should().Be(saleEntity.Id);
+        await _repo.Received(1).CreateAsync(saleEntity, Arg.Any<CancellationToken>());
+        _mapper.Received(1).Map<Sale>(command);
+        _mapper.Received(1).Map<CreateSaleResult>(saleEntity);
+        _logger.Received(1).LogInformation("sale was created");
+    }
+
+    [Fact]
+    public async Task Handle_InvalidRequest_ThrowsValidationException()
+    {
+        // Arrange
+        var invalid = new CreateSaleCommand();
+
+        // Act
+        Func<Task> act = () => _handler.Handle(invalid, CancellationToken.None);
+
+        // Assert
+        await act.Should().ThrowAsync<ArgumentNullException>();
+        await _repo.DidNotReceiveWithAnyArgs().CreateAsync(default, default);
+    }
+}
---
+++ tests/Ambev.DeveloperEvaluation.Unit/Application/SalesTests/UpdateSaleHandlerTests.cs
@@ -0,0 +1,66 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.Application.Sales.UpdateSale;
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Ambev.DeveloperEvaluation.Unit.Application.TestData;
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace Ambev.DeveloperEvaluation.Unit.Application.SalesTests;
+
+public class UpdateSaleHandlerTests
+{
+    private readonly ISaleRepository _repo = Substitute.For<ISaleRepository>();
+    private readonly IMapper _mapper = Substitute.For<IMapper>();
+    private readonly ILogger<CreateSaleHandler> _logger = Substitute.For<ILogger<CreateSaleHandler>>();
+    private readonly UpdateSaleHandler _handler;
+
+    public UpdateSaleHandlerTests()
+    {
+        _handler = new UpdateSaleHandler(_repo, _mapper, _logger);
+    }
+
+    [Fact]
+    public async Task Handle_ValidRequest_ReturnsUpdateSaleResult()
+    {
+        // Arrange
+        var cmd = SaleHandlerTestData.ValidUpdateSaleCommand();
+        var saleEntity = new Sale
+        {
+            Id = cmd.Id,
+            CustomerId = cmd.CustomerId,
+            BranchId = cmd.BranchId
+           
+        };
+
+        _mapper.Map<Sale>(cmd).Returns(saleEntity);
+        _repo.UpdateAsync(saleEntity, Arg.Any<CancellationToken>())
+             .Returns(true);
+
+        // Act
+        var result = await _handler.Handle(cmd, CancellationToken.None);
+
+        // Assert
+        result.Succeed.Should().BeTrue();
+        _mapper.Received(1).Map<Sale>(cmd);
+        await _repo.Received(1).UpdateAsync(saleEntity, Arg.Any<CancellationToken>());
+        _logger.Received(1).LogInformation("[UpdateSaleResult] Succeed!");
+    }
+
+    [Fact]
+    public async Task Handle_InvalidRequest_LogsErrorAndThrows()
+    {
+        // Arrange
+        var cmd = new UpdateSaleCommand();
+
+        // Act
+        Func<Task> act = () => _handler.Handle(cmd, CancellationToken.None);
+
+        // Assert
+        await act.Should().ThrowAsync<ArgumentNullException>();
+    }
+}
